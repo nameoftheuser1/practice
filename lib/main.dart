457938305f8dd7ae3535f2be29ae5926561eb2ec
nameoftheuser1 <103398819+nameoftheuser1@users.noreply.github.com>
2024-06-19 11:20:32 +0800

    initial part

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,20 @@ void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
     home: Scaffold(
+      backgroundColor: Colors.black45,
       appBar: AppBar(
-        title: const Text('Shikonokokoshitantan App'),
+        title: const Center(
+          child: Text('ProneHub', style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold)),
+        ),
+        backgroundColor: const Color.fromARGB(255, 255, 163, 26),
+      ),
+      body: const Center(
+        child: Text( 'Hello Mom', style: TextStyle( fontSize: 16.0, color: Colors.white )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 255, 163, 26),
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     )
   ));
